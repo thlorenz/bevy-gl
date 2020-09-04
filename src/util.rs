535258a51db54af6be796b01cdc1aaffa20f90ea
Thorsten Lorenz <thlorenz@gmx.de>
2020-09-03 19:54:18 -0600

    util: adding load texture

--- src/util.rs
+++ src/util.rs
@@ -13,3 +13,19 @@ pub fn vert_frag_shaders(
         Shader::from_glsl(ShaderStage::Fragment, from_utf8(&*frag_buffer)?),
     ))
 }
+
+pub fn load_texture_material(
+    asset_server: &AssetServer,
+    mut textures: &mut Assets<Texture>,
+    materials: &mut Assets<StandardMaterial>,
+    path: &str,
+) -> Handle<StandardMaterial> {
+    let texture_handle = asset_server.load_sync(&mut textures, path).unwrap();
+
+    let material = materials.add(StandardMaterial {
+        albedo: Color::rgb(1.0, 1.0, 1.0),
+        albedo_texture: Some(texture_handle),
+        ..Default::default()
+    });
+    material
+}
